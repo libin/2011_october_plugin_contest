e170ccbb83837423f9d9f74d8e7454e6ebb40db3
Kevin Colyar <kevin@colyar.net>
2011-09-30 11:40:59 -0700

    Updates plugin file

--- october_2011.rb
+++ october_2011.rb
@@ -1,13 +1,16 @@
 class October2011 < Plugin
   requires_version '1.0.3'
 
+  # This method gets run before your keymandorc.rb
   def before
   end
 
+  # This method gets run for each event (key stroke)
   def event_received(sequence)
     false # return true if we handled the event here
   end
 
+  # This method gets run after your keymandorc.rb
   def after
   end
 
